98d8e9c5ca4b8935786f76393174b241160ff595
JSabich <jonathansabich@protonmail.com>
2025-05-05 22:59:04 -0300

    listas

---
+++ pilotos.py
@@ -0,0 +1,2 @@
+pilotos={"Posición 1": "O. Piastri", "Posición 2": "L. Norris", "Posición 3": "M. Verstappen" }
+for posicion, nombre in pilotos.items(): print(f' {posicion}: corresponde a {nombre}')
